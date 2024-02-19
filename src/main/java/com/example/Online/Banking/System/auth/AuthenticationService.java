ce3a89ef8155a94182502d8f44b35d29976ca018
Saliha <saliha.work@gmail.com>
2024-02-19 21:28:47 +0800

    Create authentication service class

---
+++ src/main/java/com/example/Online/Banking/System/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.example.Online.Banking.System.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+}
