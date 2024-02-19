ae1150a28edec7b6ba26a6b9692b804a57dc1b46
Saliha <saliha.work@gmail.com>
2024-02-19 20:44:34 +0800

    Generate JWT token

--- src/main/java/com/example/Online/Banking/System/config/JwtService.java
+++ src/main/java/com/example/Online/Banking/System/config/JwtService.java
@@ -2,10 +2,15 @@ package com.example.Online.Banking.System.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JwtService {
@@ -18,6 +23,21 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaim, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaim)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
