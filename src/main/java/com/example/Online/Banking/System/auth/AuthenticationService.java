acda59661ca1f73e80ad0b6f86ed3eb8fa8b5ff0
Saliha <saliha.work@gmail.com>
2024-02-19 21:31:04 +0800

    Implement authenticate method

--- src/main/java/com/example/Online/Banking/System/auth/AuthenticationService.java
+++ src/main/java/com/example/Online/Banking/System/auth/AuthenticationService.java
@@ -16,6 +16,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -27,6 +28,22 @@ public class AuthenticationService {
                 .build();
 
         userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findUserByEmail(request.getEmail()).orElseThrow();
 
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
