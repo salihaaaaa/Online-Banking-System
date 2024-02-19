e6b0cb7577a3df7e1d199cef1665be3ff76f5c7c
Saliha <saliha.work@gmail.com>
2024-02-19 20:42:21 +0800

    Extract single claim from JWT

--- src/main/java/com/example/Online/Banking/System/config/JwtService.java
+++ src/main/java/com/example/Online/Banking/System/config/JwtService.java
@@ -6,9 +6,15 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
+import java.util.function.Function;
 
 public class JwtService {
     public static final String SECRET_KEY = "254575775c3425463029565e6223403f36736d2652566179615963483c";
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 
     private Claims extractAllClaims(String token) {
         return Jwts
