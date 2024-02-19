16ade2fceb89e7c0bb715081402278ecf82cc59e
Saliha <saliha.work@gmail.com>
2024-02-19 20:50:49 +0800

    Extract username from the token

--- src/main/java/com/example/Online/Banking/System/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/Online/Banking/System/config/JwtAuthenticationFilter.java
@@ -4,12 +4,19 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -28,5 +35,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         // Get the token
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
