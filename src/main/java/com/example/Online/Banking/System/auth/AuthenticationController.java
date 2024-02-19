eb36bc557f3e0d86ffc1709939bc55d48bc6e38f
Saliha <saliha.work@gmail.com>
2024-02-19 21:17:32 +0800

    Create authentication controller

---
+++ src/main/java/com/example/Online/Banking/System/auth/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.example.Online.Banking.System.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/authentication")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+}
