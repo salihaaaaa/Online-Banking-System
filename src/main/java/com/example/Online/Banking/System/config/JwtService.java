2d7f81ef45a80b57cd79433c15dd6343203f5bd6
Saliha <saliha.work@gmail.com>
2024-02-19 20:41:07 +0800

    Extract claims from JWT and implement getSignInKey method

--- src/main/java/com/example/Online/Banking/System/config/JwtService.java
+++ src/main/java/com/example/Online/Banking/System/config/JwtService.java
@@ -1,4 +1,26 @@
 package com.example.Online.Banking.System.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
 public class JwtService {
+    public static final String SECRET_KEY = "254575775c3425463029565e6223403f36736d2652566179615963483c";
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
