705f180dbe41e377c9e5cae9257fbff48c9391e4
Saliha <saliha.work@gmail.com>
2024-02-19 20:19:25 +0800

    Initial commit

---
+++ src/test/java/com/example/Online/Banking/System/OnlineBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Online.Banking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
