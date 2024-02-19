4d7913b8c5270b17d706ee28469176afc686a388
Saliha <saliha.work@gmail.com>
2024-02-19 20:36:05 +0800

    Create jwt authentication filter and check if jwt token exists

---
+++ src/main/java/com/example/Online/Banking/System/config/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package com.example.Online.Banking.System.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        // Check if token exists
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Get the token
+        jwt = authHeader.substring(7);
+    }
+}
