b0088dba72d01b273cd4e2b2f0816a006fff569b
Saliha <saliha.work@gmail.com>
2024-02-19 21:29:53 +0800

    Implement register method

--- src/main/java/com/example/Online/Banking/System/auth/AuthenticationService.java
+++ src/main/java/com/example/Online/Banking/System/auth/AuthenticationService.java
@@ -1,10 +1,36 @@
 package com.example.Online.Banking.System.auth;
 
+import com.example.Online.Banking.System.config.JwtService;
+import com.example.Online.Banking.System.user.Role;
+import com.example.Online.Banking.System.user.User;
+import com.example.Online.Banking.System.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ADMIN)
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
 }
